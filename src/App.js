411caa6e00cfc4ac611d3539a0c5fcec27487bcc
Randy <rrw.burnet@gmail.com>
2020-07-12 23:01:27 +0200

    handlestatechange

--- src/App.js
+++ src/App.js
@@ -7,21 +7,61 @@ class App extends Component {
       { title: 'the second title' },
       { title: 'the third title' },
     ],
+
+    addTodoValue: '',
   };
 
   removeItem = (title) => {
-    //you dont have acces to the state directly it's immutable you can acces it through the "this.setState({})"
     this.setState({
       tasks: this.state.tasks.filter((task) => {
-          return task.title !== title
-      })
-    })
-  }
+        return task.title !== title;
+      }),
+    });
+  };
+
+  addItem = (title) => {
+    const newTitle = {
+      title: title,
+    };
+    this.setState({
+      tasks: [...this.state.tasks, newTitle],
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      addTodoValue: e.target.value,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({
+      addTodoValue: '',
+    });
+    
+    this.addItem(this.state.addTodoValue);
+  };
 
   render() {
     return (
       <div>
-        <TasksComponent titles={this.state.tasks} removeItem={this.removeItem} />
+        <TasksComponent
+          titles={this.state.tasks}
+          removeItem={this.removeItem}
+        />
+        <form>
+          <input
+            type="text"
+            value={this.state.addTodoValue}
+            name="title"
+            placeholder="add a task"
+            onChange={(e) => this.handleChange(e)}
+          />
+          <button type="submit" onClick={(e) => this.handleSubmit(e)}>
+            Add
+          </button>
+        </form>
       </div>
     );
   }
@@ -31,13 +71,20 @@ const TasksComponent = ({ titles, removeItem }) => {
   return (
     <div>
       {titles.map((item, index) => {
-        return <Task key={index} title={item.title} index={index} removeItem={removeItem} />;
+        return (
+          <Task
+            key={index}
+            title={item.title}
+            index={index}
+            removeItem={removeItem}
+          />
+        );
       })}
     </div>
   );
 };
 
-const Task = ({ title, index, removeItem}) => {
+const Task = ({ title, index, removeItem }) => {
   return (
     <div>
       {' '}
